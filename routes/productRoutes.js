53fc8d1069ac6636712c5cdd43f88da352e9c190
codez <manishm9730@gmail.com>
2023-04-17 01:12:40 +0530

    Completed productController

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js'
-import { createProductController, getProductController, getSingleProductController } from '../controllers/productController.js'
+import { createProductController, deleteProductController, getProductController, getSingleProductController, productPhotoController, updateProductController } from '../controllers/productController.js'
 import formidable from 'express-formidable'
 
 const router = express.Router()
@@ -9,11 +9,20 @@ const router = express.Router()
 //create product
 router.post('/create-product',requireSignIn,isAdmin,formidable(), createProductController)
 
+//update product
+router.put('/update-product/:pid',requireSignIn,isAdmin,formidable(), updateProductController)
+
 //get products
 router.get('/get-product', getProductController)
 
 // get single product
 router.get('/get-product/:slug', getSingleProductController)
+
+// get photo
+router.get('/product-photo/:pid', productPhotoController)
+
+// delete product
+router.delete('/product/:pid', deleteProductController)
 
 
 export default router
